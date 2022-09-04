82ea1d426a7697ea2c14da741efb974b549c56bc
Kumaran Naidu <kumaran.naidu@gmail.com>
2022-09-04 16:33:28 +0530

    Syso for Multiple Repo

--- GitConcepts.java
+++ GitConcepts.java
@@ -2,5 +2,6 @@ public class GitConcepts{
     public static void main(String[] args) {
 		//This is main method
         System.out.println("Hello Git");
+		System.out.println("Hello Multiple Repo Git ");
     }
 }
