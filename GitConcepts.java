1ef019de287b1208d63b36f9cdb3b468c57999c8
Kumaran Naidu <kumaran.naidu@gmail.com>
2023-01-08 09:33:51 +0530

    Modified Gitconcepts and Added a file

--- GitConcepts.java
+++ GitConcepts.java
@@ -1,8 +1,7 @@
 public class GitConcepts{
     public static void main(String[] args) {
 		//This is main method
-		//Modifying comments
-        System.out.println("Hello Git");
+		System.out.println("Hello Git");
 		System.out.println("Hello Multiple Repo Git ");
 		System.out.println("Hello Branch Git ");
 		
