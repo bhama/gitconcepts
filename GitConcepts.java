67c33d344bceb2fd594632fcfa8a2d7d35e643a9
Kumaran <kumaran.naidu@gmail.com>
2025-03-13 19:14:07 +0530

    Modified Branch to Main

--- GitConcepts.java
+++ GitConcepts.java
@@ -3,7 +3,7 @@ public class GitConcepts{
 		//This is main method
 		System.out.println("Hello Git");
 		System.out.println("Hello Multiple Repo Git ");
-		System.out.println("Hello Branch Git ");
+		System.out.println("Hello Main Branch Git ");
 		
     }
 }
